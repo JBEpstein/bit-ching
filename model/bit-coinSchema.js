9d5cded7226c61ae0b2006d80c5e4ff17aa83fcf
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-20 20:43:54 -0800

    added get route and returning a databased

--- model/bit-coinSchema.js
+++ model/bit-coinSchema.js
@@ -2,29 +2,14 @@
 
 
 const mongoose = require('mongoose');
-const superagent = require('superagent');
+
 
 const currencySchema = new mongoose.Schema({
-  bitData: [{type:String}],
+  userId: {type:String},
+  apiKey: {type:String},
+  apiSecret: String,
+  version: Date,
 });
 
-currencySchema.methods.returnData = function(){
-  console.log('shit');
-  return new Promise( (resolve,reject) => {
-    this.bitData = generateData(this.userId);
-    this.save()
-  .then( user => {
-    if(!user) reject();
-    resolve(user);
-  });
-  });
-};
 
-const generateData = function(res,req){
-  console.log(res.text, 'fuck');
-  return superagent.get('https://api.cryptonator.com/api/full/btc-usd')
-  .then( res => {
-    res.text = req.bitData;
-  });
-};
 module.exports = mongoose.model('Currency', currencySchema);
