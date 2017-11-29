0ce4b47fe9d39f5ea98e5e319badb90728cd43c9
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-28 19:43:26 -0800

    now will send the email if the current price is below the buying price letting the user know to buy

--- route/sendmail.js
+++ route/sendmail.js
@@ -6,47 +6,46 @@ const Client = require('coinbase').Client;
 const Currency = require('../model/bit-coinSchema.js');
 const bearAuth = require('../lib/bearAuth.js');
 sendmailRouter.post('/sendmail', bearAuth, (req, res, next) => {
-//   let priceObj = new Client({'apiKey': 'API Key','apiSecret': 'API SECRET'});
-//   let sell = function(){
-//     priceObj.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, price) {
-//     console.log(price);
-//
-//   });
-// };
 
   let dataObj = new Client({'apiKey': 'API Key','apiSecret': 'API SECRET'});
   dataObj.getSpotPrice({'currencyPair': 'BTC-USD'},function(err,price){
-    let mail = function() {
-      sendmail({
-        from: 'no-reply@bit-ching.com',
-        to: 'ryleeandrews73@gmail.com',
-        replyTo: 'no-reply',
-        subject: 'Bit-Ching Buy Alert',
-        html: 'UserId: ' + `${req.user.id}` + ' ' + 'Price of coin: ' + `${price.data.amount}`,
-      }, function (err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      });
-    };
-    let todaysCoinPrice = 9964.04;
 
-    const mock = {
-      userId: req.user.id,
-      data: {
-        base: price.data.base,
-        amount: price.data.amount,
-        currency: price.data.currency,
+    let priceObj = new Client({'apiKey': 'API Key','apiSecret': 'API SECRET'});
+    priceObj.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, userPrice) {
+        console.log(price);
+        console.log(userPrice);
+      let mail = function() {
+        sendmail({
+          from: 'no-reply@bit-ching.com',
+          to: 'ryleeandrews73@gmail.com',
+          replyTo: 'no-reply',
+          subject: 'Bit-Ching Buy Alert',
+          html: 'UserId: ' + `${req.user.id}` + ' ' + 'Price of coin: ' + `${price.data.amount}`,
+        }, function (err, reply) {
+          console.log(err && err.stack);
+          console.dir(reply);
+        });
+      };
+
+      const mock = {
+        userId: req.user.id,
+        data: {
+          base: price.data.base,
+          amount: price.data.amount,
+          currency: price.data.currency,
+        }
+      };
+      Currency.userId = req.user.id;
+
+      (new Currency(mock)).save()
+    .then( data => {
+      if(price.data.amount < userPrice){
+        res.status(200).send(mail());
+      }else{
+        res.end();
       }
-    };
-    Currency.userId = req.user.id;
-    (new Currency(mock)).save()
-  .then( data => {
-    if(price.data.amount > todaysCoinPrice){
-      res.status(200).send(mail());
-    }else{
-      res.end();
-    }
-  })
-  .catch( err => next(err));
+    })
+      .catch( err => next(err));
+    });
   });
 });
