0eef509da6c6bf7e2fa08d8c5d22242227ff8532
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-28 18:32:17 -0800

    email now sending user id and price

--- route/sendmail.js
+++ route/sendmail.js
@@ -6,22 +6,30 @@ const Client = require('coinbase').Client;
 const Currency = require('../model/bit-coinSchema.js');
 const bearAuth = require('../lib/bearAuth.js');
 sendmailRouter.post('/sendmail', bearAuth, (req, res, next) => {
-
-  let mail = function() {
-    sendmail({
-      from: 'no-reply@bit-ching.com',
-      to: 'ryleeandrews73@gmail.com',
-      replyTo: 'no-reply',
-      subject: 'Bit-Ching Buy Alert',
-      html: 'This only works at Codefellows because of security set up!'
-    }, function (err, reply) {
-      console.log(err && err.stack);
-      console.dir(reply);
-    });
-  };
+//   let priceObj = new Client({'apiKey': 'API Key','apiSecret': 'API SECRET'});
+//   let sell = function(){
+//     priceObj.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, price) {
+//     console.log(price);
+//
+//   });
+// };
 
   let dataObj = new Client({'apiKey': 'API Key','apiSecret': 'API SECRET'});
   dataObj.getSpotPrice({'currencyPair': 'BTC-USD'},function(err,price){
+    let mail = function() {
+      sendmail({
+        from: 'no-reply@bit-ching.com',
+        to: 'ryleeandrews73@gmail.com',
+        replyTo: 'no-reply',
+        subject: 'Bit-Ching Buy Alert',
+        html: 'UserId: ' + `${req.user.id}` + ' ' + 'Price of coin: ' + `${price.data.amount}`,
+      }, function (err, reply) {
+        console.log(err && err.stack);
+        console.dir(reply);
+      });
+    };
+    let todaysCoinPrice = 9964.04;
+
     const mock = {
       userId: req.user.id,
       data: {
@@ -33,8 +41,7 @@ sendmailRouter.post('/sendmail', bearAuth, (req, res, next) => {
     Currency.userId = req.user.id;
     (new Currency(mock)).save()
   .then( data => {
-    if(price.data.amount > 1000){
-      //rylee workss
+    if(price.data.amount > todaysCoinPrice){
       res.status(200).send(mail());
     }else{
       res.end();
